d48b50e88949407827713edb9fb34f43820ac48b
calvo23232 <142857309+calvo23232@users.noreply.github.com>
2023-09-06 21:40:43 -0300

    Update cog.py

--- ballsdex/packages/players/cog.py
+++ ballsdex/packages/players/cog.py
@@ -443,7 +443,7 @@ class Players(commands.GroupCog, group_name=settings.players_group_cog_name):
                 view=DonationRequest(self.bot, interaction, countryball, new_player),
             )
             return
-
+            
         countryball.player = new_player
         countryball.trade_player = old_player
         countryball.favorite = False
@@ -455,3 +455,32 @@ class Players(commands.GroupCog, group_name=settings.players_group_cog_name):
             f"{user.mention}!"
         )
         del self.bot.locked_balls[countryball.id]
+
+
+@app_commands.command()
+    @app_commands.checks.cooldown(1, 5, key=lambda i: i.user.id)
+    async def rarity(self, interaction: discord.Interaction):
+        """
+        Shows the rarity of all asiaballs!
+        """
+        bot_countryballs = await Ball.filter(enabled=True).order_by("rarity")
+
+        entries = []
+        for countryball in bot_countryballs:
+            emoji = self.bot.get_emoji(countryball.emoji_id)
+            if emoji:
+                entries.append((f"**{countryball.country}**", f"{emoji} Rarity: {countryball.rarity}"))
+
+        if len(entries) == 0:
+            await interaction.response.send_message("No asiaballs found.", ephemeral=True)
+            return
+
+        source = FieldPageSource(entries, per_page=5, inline=False, clear_description=False)
+        source.embed.description = "Asiaballs Rarity"
+        source.embed.colour = discord.Colour.blurple()
+        source.embed.set_author(
+            name=interaction.user.display_name, icon_url=interaction.user.display_avatar.url
+        )
+
+        pages = Pages(source=source, interaction=interaction, compact=True)
+        await pages.start()
